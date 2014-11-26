a97b316fab4b36ce5a2986c9b8932cfec652336b
Nicholas Allen <nick.an.allen@gmail.com>
2014-11-26 18:21:35 -0500

    Changed stuff

--- .bin/idle.sh
+++ .bin/idle.sh
@@ -12,33 +12,33 @@ while true; do
     echo $idleTimeMillis  # just for debug purposes.
 
     if [[ $idleTimeMillis -gt $idleStageOne && $idleone = false && ! -a ~/.screenlock ]] ; then
-        echo "idle stage one"
-        echo "$(xbacklight -get) + 1" | bc > ~/.backlight
-        xbacklight -set 3 -time 5000 -steps 50 & 
-        idleone=true
-        pollingRate=0.25
-    fi
+            echo "idle stage one"
+            echo "$(xbacklight -get) + 1" | bc > ~/.backlight
+            xbacklight -set 3 -time 5000 -steps 50 & 
+            idleone=true
+            pollingRate=0.25
+        fi
 
-    if [[ $idleTimeMillis -gt $idleStageTwo && $idletwo = false && ! -a ~/.screenlock ]] ; then
-        echo "idle stage two"
-        xbacklight -set 0 
-        xset dpms force off 
-        [ "$(iwgetid -r)" -ne "$homeNetwork" ] && fuzzylock
-        idletwo=true
-        pollingRate=1
-    fi
+        if [[ $idleTimeMillis -gt $idleStageTwo && $idletwo = false && ! -a ~/.screenlock ]] ; then
+            echo "idle stage two"
+            xbacklight -set 0 
+            xset dpms force off 
+            [ "$(iwgetid -r)" -ne "$homeNetwork" ] && fuzzylock
+            idletwo=true
+            pollingRate=1
+        fi
 
-    if [[ $idleTimeMillis -lt $idleStageTwo && $idletwo = true ]] || [[ $idleTimeMillis -lt $idleStageOne && $idleone = true ]] ; then
-        echo "idle off"
-        xset dpms force on
-        killall xbacklight
-        xbacklight -set $(cat ~/.backlight)
-        echo $(xbacklight -get)
-        rm ~/.backlight
-        idleone=false
-        idletwo=false
-        pollingRate=2
-    fi
+        if [[ $idleTimeMillis -lt $idleStageTwo && $idletwo = true ]] || [[ $idleTimeMillis -lt $idleStageOne && $idleone = true ]] ; then
+            echo "idle off"
+            xset dpms force on
+            killall xbacklight
+            xbacklight -set $(cat ~/.backlight)
+            echo $(xbacklight -get)
+            rm ~/.backlight
+            idleone=false
+            idletwo=false
+            pollingRate=2
+        fi
 
     sleep $pollingRate   # polling interval
 
